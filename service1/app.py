f4ef3e81a646e98cd60d647d64401c9bac0132c0
rggaikwad <159951227+rggaikwad@users.noreply.github.com>
2025-06-02 08:45:13 +0530

    Update app.py

--- service1/app.py
+++ service1/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello from Service 1!"
+def home():
+    return "Hello from Flask on EKS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
